3c2e48729d8d9baf28e5fa76d2494617cbf36a67
arisulistio37 <arisulistio37@gmail.com>
2019-10-10 17:54:53 +0700

    Initial commit

---
+++ src/if1/pkg10118030/latihan18/kelipatan/IF110118030Latihan18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118030.latihan18.kelipatan;
+
+/**
+ *
+ * @author 
+ * NAMA     : Ari Sulistio
+ * KELAS    : IF-1
+ * NIM      : 10118030
+ * Deskripsi Program    : Program ini berisi tentang kelipatan 3.5
+ */
+public class IF110118030Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        double i;
+        i =3.5;
+        while(i<=35){
+            System.out.println(i);
+            i+=3.5;
+        }
+    }
+    
+}
